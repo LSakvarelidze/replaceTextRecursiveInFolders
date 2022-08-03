377bb8a4fdd35b65e9f02fcd66c65096e6a7a1c4
lsakvarelidze <l.sakvarelidze@hotmail.com>
2022-08-03 16:19:45 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let currentStr, replaceStr;
+
+const replaceRecursively = (folderName) => {
+    fs.readdir(folderName, { withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+
+        files.forEach((filename) => {
+            if (filename.isDirectory()) {
+                if (filename.name == 'images') {
+                    return
+                } else {
+                    replaceRecursively(`${folderName}/${filename.name}`)
+                }
+                return
+            } else {
+                fs.readFile(`${folderName}/${filename.name}`, 'utf8', function (err, data) {
+                    if (err) throw err;
+
+                    var re = new RegExp(currentStr, "g");
+                    var result = data.replace(re, replaceStr);
+
+                    fs.writeFile(`${folderName}/${filename.name}`, result, 'utf8', function (err) {
+                        if (err) return console.log(err);
+                    });
+
+                });
+            }
+        });
+    });
+    console.log('Job done')
+}
+
+rl.question('What is current name of merchant folder ? ', function (current) {
+    rl.question('What should be ? ', function (future) {
+        currentStr = current;
+        replaceStr = future;
+        rl.close();
+    });
+});
+
+rl.on('close', () => replaceRecursively(currentStr))
